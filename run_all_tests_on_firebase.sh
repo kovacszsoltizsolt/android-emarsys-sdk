59fb9d957f920f7ffb7fe651c5c595d9b0ff9b8b
Kovács Zsolt <kovacs.zsolti.zsolt@gmail.com>
2020-09-04 14:20:57 +0200

    Update run_all_tests_on_firebase.sh

--- run_all_tests_on_firebase.sh
+++ run_all_tests_on_firebase.sh
@@ -12,14 +12,13 @@ predict
 emarsys
 emarsys-sdk
 "
+FIREBASE_DEVICES="--device model=flo,version=19,locale=en,orientation=portrait --device model=hammerhead,version=21,locale=en,orientation=portrait --device model=shamu,version=22,locale=en,orientation=portrait --device model=hero2lte,version=23,locale=en,orientation=portrait --device model=griffin,version=24,locale=en,orientation=portrait --device model=cruiserlteatt,version=26,locale=en,orientation=portrait --device model=sailfish,version=27,locale=en,orientation=portrait --device model=walleye,version=28,locale=en,orientation=portrait --device model=flame,version=29,locale=en,orientation=portrait"
 set -ex
-
-curl -o /tmp/sacc_key.json $BITRISEIO_SERVICE_ACCOUNT_KEY_URL
 
 #Activate cloud client with the service account
 gcloud auth activate-service-account -q --key-file /tmp/sacc_key.json
 #Set the project's id used on Google Cloud Platform
-gcloud config set project $GCP_PROJECT
+gcloud config set project "ems-mobile-sdk"
 
 for i in $MODULES; do
    gcloud firebase test android run --type instrumentation --test "$i/build/outputs/apk/androidTest/debug/$i-debug-androidTest.apk" --app=sample/build/outputs/apk/androidTest/debug/sample-debug-androidTest.apk $FIREBASE_DEVICES --timeout 20m --quiet --project ems-mobile-sdk &
